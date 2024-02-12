4ccb6fc1e25e47748d7d8370cf9b42e40a787069
j7jhj <157136647+j7jhj@users.noreply.github.com>
2024-02-11 20:07:38 -0500

    Add files via upload

---
+++ Gang_Members.py
@@ -0,0 +1,27 @@
+Enemies = {
+    "Tyrone": {
+        "Name": 'Tyrone',
+        'HP': 200,
+        'dmg': 20,
+    },
+    "Doomsday Hong": {
+        "Name": 'Doomsday Hong',
+        'HP': 300,
+        'dmg': 30,
+    },
+    "Steampunk Willie": {
+        "Name": "Steampunk Willie",
+        "HP": 500,
+        "dmg": 45
+    },
+    "Steampunk Tyrone": {
+        "Name": "Steampunk Tyrone",
+        "HP": 600,
+        "dmg": 60
+    },
+    "Puppet Tyrone": {
+        "Name": "Puppet Tyrone",
+        "HP": 60,
+        "dmg": 0
+    }
+}
